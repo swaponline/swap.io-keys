277516e7f5b3627d787c0dc6248718532bd2bff9
alex-rak <2304alex2304@gmail.com>
2021-04-14 17:13:27 +0500

    rework allSystem

--- src/main.js
+++ src/main.js
@@ -1,27 +1,15 @@
 import Vue from 'vue'
-import vuetify from '@/plugins/vuetify'
-import UI from '@/components/UI'
 import App from './App.vue'
-import router from './router'
-import store from './store'
-import { i18n, loadLanguageAsync } from './i18n'
-import { getLanguage } from './utils/storage'
-import '@/assets/scss/base.scss'
+import messageHandler from './messageHandler'
+import windowParentPostMessage from './windowParentPostMessage'
 
-Vue.config.productionTip = false
-Vue.use(UI)
-const lang = getLanguage()
+messageHandler()
+windowParentPostMessage({ key: 'createWindow' })
 
 // Не разрешаем обычное открытие, только в iframe
 if (window.top !== window.self) {
-  loadLanguageAsync(lang).then(() => {
-    /* eslint-disable vue/require-name-property */
-    new Vue({
-      i18n,
-      router,
-      vuetify,
-      store,
-      render: h => h(App)
-    }).$mount('#app')
-  })
+  /* eslint-disable vue/require-name-property */
+  new Vue({
+    render: h => h(App)
+  }).$mount('#app')
 }
