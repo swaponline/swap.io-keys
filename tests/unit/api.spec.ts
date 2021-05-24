2b863495916a1694de4db7a3ab831d2e22f47da7
iower <iower@tutanota.com>
2021-05-24 15:28:41 +0500

    Add BTC, LTC, ETH account test references (#18)

---
+++ tests/unit/api.spec.ts
@@ -0,0 +1,15 @@
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+import { mnemonic, references } from './references'
+
+
+/*describe('HelloWorld.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg }
+    })
+    expect(wrapper.text()).toMatch(msg)
+  })
+})*/
+
