7ef3cf9fada3559cb491e7020f10a19d13b24d22
Denis Sannikov <smaylik911@gmail.com>
2021-06-16 14:35:07 +0700

    correction of the layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,48 @@
 <template>
   <div id="app">
-    <router-view />
+    <media-query-provider :queries="queries">
+      <router-view />
+    </media-query-provider>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { MediaQueryProvider } from 'vue-component-media-queries'
+
+interface Queries {
+  desktop: string
+  tablet: string
+  phone: string
+  smallPhone: string
+}
+
+const queries: Queries = {
+  desktop: '(min-width: 1065px)',
+  tablet: '(max-width: 1064px)',
+  phone: '(max-width: 480px)',
+  smallPhone: '(max-width: 320px)'
+}
+
+type Data = {
+  queries: Record<string, string>
+}
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+    MediaQueryProvider
+  },
+  data(): Data {
+    return {
+      queries
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+#app {
+  height: 100%;
+}
+</style>
