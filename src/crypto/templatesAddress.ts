77fd3be4179dd9c94f38f7bd0948344cfb25e20e
shendel <im_shendel@inbox.ru>
2021-09-08 19:39:13 +0300

    add kusama

--- src/crypto/templatesAddress.ts
+++ src/crypto/templatesAddress.ts
@@ -4,13 +4,15 @@ import handshakeDefault from './templates/address/handshake/default'
 import tronDefault from './templates/address/tron/default'
 import stellarDefault from './templates/address/stellar/default'
 import polkadotDefault from './templates/address/polkadot/default'
+import kusamaDefault from './templates/address/kusama/default'
 
 
 const templates = {
   'handshake/default': handshakeDefault,
   'tron/default': tronDefault,
   'stellar/default': stellarDefault,
-  'polkadot/default': polkadotDefault
+  'polkadot/default': polkadotDefault,
+  'kusama/default': kusamaDefault
 }
 
 export default templates
