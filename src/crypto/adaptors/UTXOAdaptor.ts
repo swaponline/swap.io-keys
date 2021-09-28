7e1c76cf407579339c2ae87da12cae4eada09e62
shendel <im_shendel@inbox.ru>
2021-09-28 18:49:55 +0300

    utxo like - sign message (draft)

--- src/crypto/adaptors/UTXOAdaptor.ts
+++ src/crypto/adaptors/UTXOAdaptor.ts
@@ -4,6 +4,9 @@ import BaseWallet from '../wallets/BaseWallet'
 import UTXOWallet from '../wallets/UTXOWallet'
 import { ISignedMessage } from '../types'
 
+import * as bitcoin from 'bitcoinjs-lib'
+import * as bitcoinMessage from 'bitcoinjs-message'
+
 type Seed = Buffer
 
 class UTXOAdaptor extends BaseAdaptor {
@@ -26,7 +29,29 @@ class UTXOAdaptor extends BaseAdaptor {
     return wallet
   }
 
-  public signMessage(options = {}): ISignedMessage|false {
+  public signMessage(options): ISignedMessage|false {
+    const {
+      message,
+    } = options
+    //@ts-ignore
+    const signWallet: BaseWallet = this.createWallet(options)
+
+    ///
+    const FLOTESTNET = {
+      messagePrefix: '\x19FLO testnet Signed Message:\n',
+      bip32: {
+        public: 0x013440e2,
+        private: 0x01343c23
+      },
+      pubKeyHash: 0x73,
+      scriptHash: 0xc6,
+      wif: 0xef
+    }
+    // @ts-ignore
+    var keyPair = bitcoin.ECPair.fromWIF('cRgnQe9MUu1JznntrLaoQpB476M8PURvXVQB5R2eqms5tXnzNsrr', FLOTESTNET)
+    // @ts-ignore
+    var signature = bitcoinMessage.sign(message, keyPair.privateKey, keyPair.compressed)
+
     return false
   }
 
