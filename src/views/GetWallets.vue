4ff002f1acdfbdd3ca4af692ecc20b7c1a56ddc7
shendel <im_shendel@inbox.ru>
2021-11-04 15:05:40 +0300

    get wallets of profile (draft)

---
+++ src/views/GetWallets.vue
@@ -0,0 +1,41 @@
+<template></template>
+
+<script lang="ts">
+/* eslint-disable */
+import Vue from 'vue'
+import windowParentPostMessage from '@/windowParentPostMessage'
+import CryptoInterface from '@/crypto/interface'
+
+type Data = {}
+
+export default Vue.extend({
+  name: 'GetWallets',
+  mounted(): void {
+    window.addEventListener('message', event => {
+      if (event.data && event.data.key && event.data.key === `GetWalletsWindow`) {
+        if (event.data && event.data.type) {
+          if (event.data.type === `GetWallets`) {
+            const data = event.data.data
+            windowParentPostMessage({
+              key: 'GetWalletsWindow',
+              message: {}
+            })
+          }
+        }
+      }
+    })
+    windowParentPostMessage({
+      key: 'GetWalletsWindow',
+      message: {
+        type: 'iframeInited'
+      }
+    })
+  },
+  data(): Data {
+    return {}
+  },
+  methods: {}
+})
+</script>
+
+<style lang="scss"></style>
