5f1dd454c89c28d916127d902f2a07fb192b419e
Denis Sannikov <smaylik911@gmail.com>
2021-12-07 16:01:17 +0700

    save the selected theme after reloading the iframe

---
+++ src/constants/messageTypes.ts
@@ -0,0 +1,15 @@
+export const iframeMessageTypes = {
+  IFRAME_LOADED: 'iframeLoaded',
+  IFRAME_RENDERED: 'iframeRendered',
+  SET_DEFAULT_STATE_IFRAME: 'setDefaultStateIframe'
+}
+
+export const profileMessageTypes = {
+  GET_PROFILES: 'iframeGetProfiles',
+  THEME_SELECTED: 'themeSelected',
+  PROFILE_CREATED: 'profileCreated',
+  CREATION_CANCELLED: 'creationCancelled',
+  RECOVER_CANCELED: 'recoverCanceled',
+  PROFILE_RECOVERED: 'profileRecovered',
+  SET_APP_THEME: 'setAppTheme'
+}
