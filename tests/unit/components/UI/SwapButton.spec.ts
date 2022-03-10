01f12fb55d3ebb60d6f4d56b69fcae0d8f32fcc8
Tatyana <syrtsova.tanya@gmail.com>
2022-03-10 17:40:52 +0700

    feat: add button tests

---
+++ tests/unit/components/UI/SwapButton.spec.ts
@@ -0,0 +1,48 @@
+import SwapButton from '@/components/UI/SwapButton.vue'
+import { shallowMount } from '@vue/test-utils'
+
+const slotText = 'slot test'
+
+describe('Form password', () => {
+  let wrapper
+
+  const createComponent = ({ propsData = {}, provide = {} } = {}) => {
+    wrapper = shallowMount(SwapButton, {
+      propsData: {
+        ...propsData
+      },
+      provide: {
+        mediaQueries: { mobile: false },
+        ...provide
+      },
+      slots: {
+        default: slotText
+      }
+    })
+  }
+
+  it('renders slot', () => {
+    createComponent()
+
+    expect(wrapper.text()).toBe(slotText)
+  })
+
+  it('renders tooltip', () => {
+    createComponent({ propsData: { tooltip: 'Tooltip' } })
+
+    const buttonTooltip = wrapper.find('[data-testid="button-tooltip"]')
+
+    expect(buttonTooltip.exists()).toBe(true)
+  })
+
+  it.each`
+    propName
+    ${'round'}
+    ${'text'}
+    ${'disabled'}
+  `('has class for styles $propName', ({ propName }) => {
+    createComponent({ propsData: { [propName]: true } })
+
+    expect(wrapper.element.classList.value.includes(propName)).toBe(true)
+  })
+})
